5bcf715c9060d458e15c86a6cbea63451ec53401
ozzy <ozzy@cydeo.com>
2022-11-01 12:46:08 -0400

    Part-1

--- src/main/java/com/cydeo/converter/RoleDtoConverter.java
+++ src/main/java/com/cydeo/converter/RoleDtoConverter.java
@@ -8,22 +8,26 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationPropertiesBinding
 public class RoleDtoConverter implements Converter<String, RoleDTO> {
-
-    RoleService roleService;
-
-    public RoleDtoConverter(RoleService roleService) {
-        this.roleService = roleService;
-    }
-
     @Override
     public RoleDTO convert(String source) {
-
-        if (source == null || source.equals("")) {  //  Select  -> ""
-            return null;
-        }
-
-        return roleService.findById(Long.parseLong(source));
-
+        return null;
     }
+
+//    RoleService roleService;
+//
+//    public RoleDtoConverter(RoleService roleService) {
+//        this.roleService = roleService;
+//    }
+//
+//    @Override
+//    public RoleDTO convert(String source) {
+//
+//        if (source == null || source.equals("")) {  //  Select  -> ""
+//            return null;
+//        }
+//
+//        return roleService.findById(Long.parseLong(source));
+//
+//    }
 
 }
